4a0cf4d1f4078774e3c6f08e3d33ab6a7af8db81
Andrew Van Slaars <avanslaars@gmail.com>
2018-04-21 20:18:23 -0400

    Payload creator function

--- src/reducer.spec.js
+++ src/reducer.spec.js
@@ -2,27 +2,32 @@ import reducer from './reducer'
 
 describe('Todo Reducer', () => {
   test('returns a state object', () => {
-    const result = reducer(undefined, {type:'ANYTHING'})
+    const result = reducer(undefined, { type: 'ANYTHING' })
     expect(result).toBeDefined()
   })
 
   test('adds a todo', () => {
     const startState = {
+      currentTodo: '',
       todos: [
-        {id: 1, name:'Create Static UI', isComplete: true},
-        {id: 2, name:'Create Initial State', isComplete: false},
-        {id: 3, name:'Use state to render UI', isComplete: false}
+        { id: 1, name: 'Create Static UI', isComplete: true },
+        { id: 2, name: 'Create Initial State', isComplete: false },
+        { id: 3, name: 'Use state to render UI', isComplete: false }
       ]
     }
     const expectedState = {
+      currentTodo: '',
       todos: [
-        {id: 1, name:'Create Static UI', isComplete: true},
-        {id: 2, name:'Create Initial State', isComplete: false},
-        {id: 3, name:'Use state to render UI', isComplete: false},
-        {id: 4, name:'Added todo', isComplete: false}
+        { id: 1, name: 'Create Static UI', isComplete: true },
+        { id: 2, name: 'Create Initial State', isComplete: false },
+        { id: 3, name: 'Use state to render UI', isComplete: false },
+        { id: 4, name: 'Added todo', isComplete: false }
       ]
     }
-    const action = {type:'TODO_ADD', payload: {id: 4, name:'Added todo', isComplete: false}}
+    const action = {
+      type: 'ADD_TODO',
+      payload: { id: 4, name: 'Added todo', isComplete: false }
+    }
     const result = reducer(startState, action)
     expect(result).toEqual(expectedState)
   })
@@ -38,7 +43,10 @@ describe('Todo Reducer', () => {
       currentTodo: 'abc'
     }
 
-    const result = reducer(startState, {type: 'CURRENT_UPDATE', payload: 'abc'})
+    const result = reducer(startState, {
+      type: 'UPDATE_CURRENT',
+      payload: 'abc'
+    })
     expect(result).toEqual(expectedState)
   })
 })
